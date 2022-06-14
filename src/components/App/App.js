7d545c1913d360a3cf44ca26e70051dd095185d5
Artan Myrtolli <artanmyrtolli@gmail.com>
2022-06-14 11:38:01 -0400

    Bug free, home-y

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import {getOrders} from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
+import { useState, useEffect } from 'react/cjs/react.development';
 
-class App extends Component {
-  constructor(props) {
-    super();
-  }
 
-  componentDidMount() {
+
+const App = () => {
+  const [orders, setOrders] = useState([])
+
+  useEffect(() => {
     getOrders()
+      .then(data => setOrders(data.orders))
       .catch(err => console.error('Error fetching:', err));
-  }
+  }, [])
 
-  render() {
+  
     return (
       <main className="App">
         <header>
           <h1>Burrito Builder</h1>
           <OrderForm />
         </header>
-
-        <Orders orders={this.state.orders}/>
+        <Orders orders={orders}/>
       </main>
     );
-  }
 }
 
 
